6240ad0bca53c63f6a4cbb5817e518e457754fe0
stetimi <123815784+stetimi@users.noreply.github.com>
2026-03-15 21:45:32 +0800

    Runner for aoc puzzles

---
+++ days/day1.go
@@ -0,0 +1,9 @@
+package days
+
+import (
+	"sjbtimdan/aoc2025-go/utils"
+)
+
+func Day1(contents []byte) utils.Answers {
+	return utils.TodoAnswers()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module sjbtimdan/aoc2025-go
+
+go 1.26.1
