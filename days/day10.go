9ea796cef48d255c37c71bf348d68f6a39e74b46
stetimi <123815784+stetimi@users.noreply.github.com>
2026-03-24 21:48:34 +0800

    Day 10

--- days/day10.go
+++ days/day10.go
@@ -1,7 +1,56 @@
 package days
 
-import "sjbtimdan/aoc2025-go/utils"
+import (
+	"bytes"
+	"sjbtimdan/aoc2025-go/utils"
+)
 
 func Day10(contents []byte) utils.Answers {
 	return utils.StringAnswers("TODO", "TODO")
+}
+
+type Machine struct {
+	desiredState uint16
+	buttons      []uint16
+}
+
+func parseMachines(contents []byte) []Machine {
+	lines := bytes.Split(contents, []byte("\n"))
+	machines := []Machine{}
+	for line := range lines {
+		parts := bytes.Split(lines[line], []byte(" "))
+		desiredStateBytesIncludingBrackets := parts[0]
+		desiredStateBytes := desiredStateBytesIncludingBrackets[1 : len(desiredStateBytesIncludingBrackets)-1]
+		desiredState := uint16(0)
+		desiredStateBytesLen := len(desiredStateBytes)
+		for i := 0; i < len(desiredStateBytes); i++ {
+			desiredStateBit := stateByteToBit(desiredStateBytes[len(desiredStateBytes)-1-i])
+			desiredState = desiredState*2 + desiredStateBit
+		}
+		buttonsBytesArray := parts[1 : len(parts)-1]
+		buttons := []uint16{}
+		for _, buttonsBytesWithParens := range buttonsBytesArray {
+			button := uint16(0)
+			buttonBytes := bytes.Split(buttonsBytesWithParens[1:len(buttonsBytesWithParens)-1], []byte(","))
+			for _, buttonByte := range buttonBytes {
+				bitIndex := desiredStateBytesLen - (int(buttonByte[0]) - '0') - 1
+				button += 1 << bitIndex
+			}
+			buttons = append(buttons, button)
+		}
+		machine := Machine{
+			desiredState: desiredState,
+			buttons:      buttons,
+		}
+		machines = append(machines, machine)
+	}
+	return machines
+}
+
+func stateByteToBit(b byte) uint16 {
+	if b == '#' {
+		return 1
+	} else {
+		return 0
+	}
 }
--- days/day10_test.go
+++ days/day10_test.go
@@ -11,7 +11,17 @@ import (
 var day10_file []byte
 
 func TestDay10(t *testing.T) {
-	result := Day10(day8_file)
+	result := Day10(day10_file)
 	must.Eq(t, "TODO", result.Part1)
 	must.Eq(t, "TODO", result.Part2)
+}
+
+func TestParseMachines(t *testing.T) {
+	machines := parseMachines([]byte("[.##.] (3) (1,3) (2) {3,5,4,7}"))
+	must.Len(t, 1, machines)
+	expected := Machine{
+		desiredState: 0b0110,
+		buttons:      []uint16{0b0001, 0b0101, 0b0010},
+	}
+	must.Eq(t, expected, machines[0])
 }
